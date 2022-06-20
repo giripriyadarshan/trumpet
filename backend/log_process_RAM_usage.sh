8c4b2ee3e15c88b1c21f014b3512648ea1ab76d5
giripriyadarshan <43517605+giripriyadarshan@users.noreply.github.com>
2022-06-20 17:58:06 +0530

    optional ram usage logger for debugging

---
+++ backend/log_process_RAM_usage.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "RAM usage as on $(date)" >> memory_usage.out
+
+for bin in "$@"
+do
+    process_id="$(pgrep "${bin}")"
+    memory_usage_in_kb="$(pmap "${process_id}" | tail -n 1 | awk '{print $2}' | rev | cut -c2- | rev)"
+    memory_usage_in_mb="$((memory_usage_in_kb / 1024))"
+    echo "${bin} ${memory_usage_in_kb}kb ${memory_usage_in_mb}mb" >> memory_usage.out
+done
+
+echo "============================================================" >> memory_usage.out
+
+cat memory_usage.out
